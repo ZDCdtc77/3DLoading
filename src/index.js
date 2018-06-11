90930c5008e0b4a84a877cc18124964f9c09a4fb
ZDCdtc77 <379192053@qq.com>
2018-06-12 00:11:30 +0800

    Signed-off-by: ZDCdtc77 <379192053@qq.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("3dloading")
+
+// 检验是否浏览器环境
+try {
+  document
+} catch (ex) {
+  throw new Error('请在浏览器环境下运行')
+} 
+
+alert("3dloading");
+
+let triloading={};
+
+// 返回
+export default (window.triloading || triloading)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname,
-        filename: './release/bundle.js'
+        filename: './release/bundle.js',
+        library: 'triloading',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     module: {
         rules: [{
