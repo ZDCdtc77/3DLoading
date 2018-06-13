ee8563f930ef6ec511318de2af010f9d8bd22f36
zhangyilin <379192053@qq.com>
2018-06-13 16:58:11 +0800

    dom

--- src/index.js
+++ src/index.js
@@ -11,7 +11,19 @@ try {
 let triloading = {
     load(option){
         console.log("triloading begin");
-        //document.getElementsByTagName("body");
+
+        let slotMask=document.createElement('div');
+        slotMask.innerHTML="<div class='tri_mask'></div>";
+        let eMask=slotMask.children[0];
+
+        let slotPop=document.createElement('div');
+        slotPop.innerHTML="<div class='tri_pop'></div>";
+        let ePop=slotPop.children[0];
+
+        eMask.appendChild(ePop);
+        document.body.appendChild(eMask);
+
+
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
         var renderer = new THREE.WebGLRenderer();
